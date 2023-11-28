9ddd27a1e0ab92ed6576a006b4bdee0513525b41
leduchien892001 <leduchien8901@gmail.com>
2023-11-28 21:41:35 +0700

    feat: test script

--- src/balance_orai_testnet.js
+++ src/balance_orai_testnet.js
@@ -11,17 +11,18 @@ const httpGet = async (url) => {
 
 const main = async (input) => {
     const responses = [];
-    let userAccount = JSON.parse(input)[0];
+    // let userAccount = JSON.parse(input)[0];
+    const userAccount = "orai1u0vfsjqh0uztlmlwv9cswggn5xkvrt4sayaxme";
     const url = `https://lcd.testnet.orai.io/cosmos/bank/v1beta1/balances/${userAccount}`;
     const result = await httpGet(url);
     for (let objectStruct of result.balances) {
-        if(objectStruct.denom == "orai"){
+        if (objectStruct.denom == "orai") {
             responses.push({
                 account: userAccount,
                 amounts: [Number(objectStruct.amount).toFixed(8).toString()]
             });
         }
-    }   
+    }
     console.log(JSON.stringify(responses))
 };
 
