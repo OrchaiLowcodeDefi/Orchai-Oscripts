c587583c63176c28c36368eca024e1748d78d523
leduchien892001 <leduchien8901@gmail.com>
2023-11-29 13:47:31 +0700

    fix: unstakingbnb test v1

--- src/unstaking_bnb.js
+++ src/unstaking_bnb.js
@@ -1,108 +1,111 @@
 import _ from "https://deno.land/std@0.120.0/node/module.ts";
-import { ethers } from "npm:ethers@5.5.1";
-import { CosmWasmClient } from "npm:@cosmjs/cosmwasm-stargate@0.28.13"
+// import { ethers } from "npm:ethers@5.5.1";
+// import { CosmWasmClient } from "npm:@cosmjs/cosmwasm-stargate@0.28.13"
 
-const stakingAbi = [
-  {
-    "inputs": [],
-    "name": "lastUnstakingRequestId",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "unstakingRequests",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "user",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "ts",
-        "type": "uint256"
-      },
-      {
-        "internalType": "bool",
-        "name": "isClaimed",
-        "type": "bool"
-      },
-      {
-        "internalType": "uint256",
-        "name": "returnAmount",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-]
+// const stakingAbi = [
+//   {
+//     "inputs": [],
+//     "name": "lastUnstakingRequestId",
+//     "outputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "uint256",
+//         "name": "",
+//         "type": "uint256"
+//       }
+//     ],
+//     "name": "unstakingRequests",
+//     "outputs": [
+//       {
+//         "internalType": "address",
+//         "name": "user",
+//         "type": "address"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "amount",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "ts",
+//         "type": "uint256"
+//       },
+//       {
+//         "internalType": "bool",
+//         "name": "isClaimed",
+//         "type": "bool"
+//       },
+//       {
+//         "internalType": "uint256",
+//         "name": "returnAmount",
+//         "type": "uint256"
+//       }
+//     ],
+//     "stateMutability": "view",
+//     "type": "function"
+//   },
+// ]
 
-const rpcProvider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.binance.org:443");
-const stakingContract = new ethers.Contract(
-  "0xa5263e756234d4d516930dc07290ef1f35e15111",
-  stakingAbi,
-  rpcProvider
-)
+// const rpcProvider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.binance.org:443");
+// const stakingContract = new ethers.Contract(
+//   "0xa5263e756234d4d516930dc07290ef1f35e15111",
+//   stakingAbi,
+//   rpcProvider
+// )
 
-async function getLastUnstakingIdBNB() {
-  return await stakingContract.lastUnstakingRequestId();
-}
+// async function getLastUnstakingIdBNB() {
+//   return await stakingContract.lastUnstakingRequestId();
+// }
 
-async function getLastUnstakingIdOraichain() {
-  const client = await CosmWasmClient.connect("https://rpc.orai.io");
-  const result = await client.queryContractSmart(
-    "orai17sy5njqjt2skvk3d9pxtywsvjf2rasnfhkptsg8xc57v35tdkluqhd3t5l",
-    {
-      last_unstaking_id: {},
-    }
-  );
-  return result;
-}
+// async function getLastUnstakingIdOraichain() {
+//   const client = await CosmWasmClient.connect("https://rpc.orai.io");
+//   const result = await client.queryContractSmart(
+//     "orai17sy5njqjt2skvk3d9pxtywsvjf2rasnfhkptsg8xc57v35tdkluqhd3t5l",
+//     {
+//       last_unstaking_id: {},
+//     }
+//   );
+//   return result;
+// }
 
 const main = async (argvParams) => {
-  const [lastUnstakingIdBNB, lastUnstakingIdOraichain] = await Promise.all([
-    getLastUnstakingIdBNB(),
-    getLastUnstakingIdOraichain(),
-  ]);
+  console.log(argvParams);
+  // const [lastUnstakingIdBNB, lastUnstakingIdOraichain] = await Promise.all([
+  //   getLastUnstakingIdBNB(),
+  //   getLastUnstakingIdOraichain(),
+  // ]);
 
-  let requests = [];
+  // console.log(lastUnstakingIdBNB, lastUnstakingIdOraichain);
 
-  let promises = [];
+  // let requests = [];
 
-  if (lastUnstakingIdBNB > lastUnstakingIdOraichain) {
-    for (let i = lastUnstakingIdOraichain; i <= lastUnstakingIdBNB; ++i) {
-      let promise = (async () => {
-        let unstakeRequest = await stakingContract.unstakingRequests(i);
-        return {
-          id: String(i),
-          amount: String(Number(unstakeRequest.amount) / Number(1e12)),
-        };
-      });
-      promises.push(promise);
-    }
-  }
-  requests = await Promise.all(promises);
-  console.log(JSON.stringify(requests));
+  // let promises = [];
+
+  // if (lastUnstakingIdBNB > lastUnstakingIdOraichain) {
+  //   for (let i = lastUnstakingIdOraichain; i <= lastUnstakingIdBNB; ++i) {
+  //     let promise = (async () => {
+  //       let unstakeRequest = await stakingContract.unstakingRequests(i);
+  //       return {
+  //         id: String(i),
+  //         amount: String(Number(unstakeRequest.amount) / Number(1e12)),
+  //       };
+  //     });
+  //     promises.push(promise);
+  //   }
+  // }
+  // requests = await Promise.all(promises);
+  // console.log(JSON.stringify(requests));
 }
 
 main(...process.argv.slice(2))
