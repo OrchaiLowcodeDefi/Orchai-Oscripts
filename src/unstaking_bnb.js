ff02404bf8a4deea8381f49c35f2cf350db6fac5
hienld <leduchien8901@gmail.com>
2024-01-12 13:37:13 +0700

    chore: rename unstaking file

--- src/unstaking_bnb.js
+++ src/unstaking_bnb.js
@@ -82,7 +82,7 @@ const main = async () => {
   let promises = [];
 
   if (lastUnstakingIdBNB > lastUnstakingIdOraichain) {
-    for (let i = 0; i <= lastUnstakingIdBNB; ++i) {
+    for (let i = lastUnstakingIdOraichain; i <= lastUnstakingIdBNB; ++i) {
       let promise = (async () => {
         let unstakeRequest = await stakingContract.unstakingRequests(i);
         return {
